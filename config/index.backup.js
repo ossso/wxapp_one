567b7b9338c2fbb80cb72fa21aa6de405815ff72
橙色阳光 <1122@hizhou.cn>
2018-05-28 22:17:58 +0800

    更新说明

--- config/index.backup.js
+++ config/index.backup.js
@@ -3,5 +3,5 @@ exports = module.exports = {
     "host": "https://domain.com/",
     "system": "zbp",
     "urlrule": "0", // 0伪静态地址规则 || 1动态地址规则 || 2index.php模式伪静态地址规则
-    "parse_type": "1", // 0伪静态地址规则 || 1动态地址规则 || 2index.php模式伪静态地址规则
+    "parse_type": "1", // 富文本解析器选择 默认为 "1" || 采用修改版wxParse 为 "2"
 }
